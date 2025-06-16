29d5fbe480e6fe52c8d195bbc2e895b40f2c06d5
AkashBadgujar2408 <akashbadgujar1999@gmail.com>
2025-06-16 22:47:00 +0530

    Add project files.

---
+++ AdoLite/Abstractions/IDatabase.cs
@@ -0,0 +1,16 @@
+﻿
+using Microsoft.Data.SqlClient;
+using System.Data;
+using System.Data.Common;
+
+namespace AdoLite.Abstractions;
+
+public interface IDatabase
+{
+    Task<IDataReader> ExecuteReaderAsync(DbCommand dbCommand);
+    Task<IDataReader> ExecuteReaderAsync(string storedProcedure);
+    Task<IDataReader> ExecuteReaderAsync(string storedProcedure, params SqlParameter[] parameters);
+    Task<int> ExecuteNonQueryAsync(DbCommand dbCommand);
+    Task<int> ExecuteNonQueryAsync(string storedProcedure);
+    Task<int> ExecuteNonQueryAsync(string storedProcedure, params SqlParameter[] parameters);
+}
